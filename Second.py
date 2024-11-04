b923c47fac5f97938338565b073c2a63431dc1e2
Osinkin_Vlad <vosinkind@gmail.com>
2024-11-04 09:45:21 +0300

    Решили задачу 3

--- Second.py
+++ Second.py
@@ -20,17 +20,31 @@
 #     print(vcards_new[item], end=' ')
 # print(']')
 
-# Задача 2. Кино
-print('Задача 2.')
-films = ['Крепкий орешек', 'Назад в будущее', 
-        'Таксист', 'Леон', 'Богемская рапсодия', 'Город грехов', 
-        'Мементо', 'Отступники', 'Деревня']
-my_list = []
-movie_check = int(input('Сколько фильмов вы хотите добавить? '))
-for _ in range (movie_check):
-    movie = (input('Введите название фильма: '))
-    if movie in films:
-        my_list.append(movie)
-    else:
-        print(f'Error: фильма {movie} у нас нет')
-print(f'Ваш список любимых фильмов: {my_list}')
+# # Задача 2. Кино
+# print('Задача 2.')
+# films = ['Крепкий орешек', 'Назад в будущее', 
+#         'Таксист', 'Леон', 'Богемская рапсодия', 'Город грехов', 
+#         'Мементо', 'Отступники', 'Деревня']
+# my_list = []
+# movie_check = int(input('Сколько фильмов вы хотите добавить? '))
+# for _ in range (movie_check):
+#     movie = (input('Введите название фильма: '))
+#     if movie in films:
+#         my_list.append(movie)
+#     else:
+#         print(f'Error: фильма {movie} у нас нет')
+# print(f'Ваш список любимых фильмов: {my_list}')
+
+# # # Задача 3. Сортировка
+# print('Задача 3.')
+# N = int(input('Введите количество N чисел:'))
+# list = []
+# for n in range (N):
+#     list.append(int(input(f'Введите число {n + 1}: ')))
+# # list = [1,4,-3,0,10]
+# print('Изначальный список:', list)
+# for i in range (len(list)-1):
+#     for j in range (len(list)-1-i):
+#         if list[j] > list[j+1]:
+#             list[j], list[j+1] = list[j+1], list[j]
+# print('Отсортированный список:', list)
