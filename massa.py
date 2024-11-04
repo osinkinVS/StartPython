dbca4d8b7e11ec8d4be34e699227cc13c505e52e
Osinkin_Vlad <vosinkind@gmail.com>
2024-11-04 18:23:39 +0300

    Решили задачу 3 со словараями

--- massa.py
+++ massa.py
@@ -25,19 +25,35 @@
 # set_new = set(array_1) - set(array_2) - set(array_3)
 # print('Решение со множеством: ', *set_new)
 
-# Задача 2. Палиндром
-print('Задача 2.')
-def is_poly(string):
-    char_dict = dict()
-    for i_sym in string:
-        char_dict[i_sym] = char_dict.get(i_sym, 0) + 1
-    odd_count = 0 
-    for i_value in char_dict.values():
-        if i_value % 2 != 0:
-            odd_count +=1
-    return odd_count <= 1 
-my_string = input('Введите строку: ')
-if is_poly(my_string):
-    print('Можно сделать палиндром')
-else:
-    print('Нельзя сделать палиндром')
+# # Задача 2. Палиндром
+# print('Задача 2.')
+# def is_poly(string):
+#     char_dict = dict()
+#     for i_sym in string:
+#         char_dict[i_sym] = char_dict.get(i_sym, 0) + 1
+#     odd_count = 0 
+#     for i_value in char_dict.values():
+#         if i_value % 2 != 0:
+#             odd_count +=1
+#     return odd_count <= 1 
+# my_string = input('Введите строку: ').lower()
+# if is_poly(my_string):
+#     print('Можно сделать палиндром')
+# else:
+#     print('Нельзя сделать палиндром')
+
+# Задача 3. Словарь синонимов
+print('Задача 3.')
+synonyms_dict = dict()
+par = int(input('Введите количество пар слов: '))
+for i in range(par):
+    first, second = input(f'{i +1} пара:').lower().split(' - ')
+    synonyms_dict[first] = second
+    synonyms_dict[second] = first
+while True:
+    word = input('введите слово: ').lower()
+    if word in synonyms_dict:
+        print('Синоним: ', synonyms_dict[word].capitalize())
+        break
+    else:
+        print('Такого слова в словаре нет.')
