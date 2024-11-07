58678701c88f6b02fcf957ea22b5a46b8243ad31
Osinkin_Vlad <vosinkind@gmail.com>
2024-11-07 18:57:24 +0300

    Переписал 3 задачу

--- 3_lesson.py
+++ 3_lesson.py
@@ -93,3 +93,21 @@
 #     sites.append(new_site)
 #     for i_site in sites:
 #         display_struct(i_site)
+
+# # Задача 3. Продвинутая функция сум
+# print('Задача 3.')
+def my_sum(*args):
+    total_sum = 0 # Инициализация переменной для хранения суммы
+    for i_elem in args:
+# Проверка, является ли элемент целым числом
+        if isinstance(i_elem, int):
+            total_sum += i_elem # Добавление числа к общей сумме
+# Проверка, является ли элемент списком или кортежем
+        elif isinstance(i_elem, (list, tuple)):
+# Рекурсивный вызов функции для суммирования элементов внутри списка или кортежа
+            for x in i_elem:
+                total_sum += my_sum(x)
+    return total_sum
+# Основной код для тестирования
+print(my_sum([[1, 2, [3]], [1], 3])) # Ожидаемый результат: 10
+print(my_sum(1, 2, 3, 4, 5)) # Ожидаемый результат: 15
