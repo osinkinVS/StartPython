1f855762f7855e63dd237d0139cabad05da77d46
Osinkin_Vlad <vosinkind@gmail.com>
2024-11-11 12:37:36 +0300

    Решили 2 задачу дз6

--- six.py
+++ six.py
@@ -6,4 +6,15 @@
 # print('Список гласных букв: ', vowels)
 # print('Длина списка:', len(vowels))
 
-
+# # Задача 2. Случаные соревнования 
+# print('Задача 2.')
+# import random
+# first = [round(random.uniform(5, 10), 2) for _ in range(20)]
+# second = [round(random.uniform(5, 10), 2) for _ in range(20)]
+# win = [
+#     first[i] if first[i] > second [i]
+#     else second[i]
+#     for i in range(20)]
+# print('Первая команда:', first)
+# print('Вторая команда:', second)
+# print('Победители тура:', win)
