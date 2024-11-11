1cbf0d7c0b0096732e22ee6d01cb995d131d93e3
Osinkin_Vlad <vosinkind@gmail.com>
2024-11-11 13:22:14 +0300

    Закончили дз6

--- six.py
+++ six.py
@@ -28,3 +28,32 @@
 # second_answer = [[value, value + 4, value + 8] for value in range(1,
 # 5)]
 # print(second_answer)
+
+# # Задача 4. Список списков
+# print('Задача 4.')
+# nice_list = [[[1, 2, 3], [4, 5, 6], [7, 8, 9]], [[10, 11, 12], [13, 14, 15], [16, 17, 18]]]
+# output = [digit
+#     for each_list in nice_list
+#     for each_list_2 in each_list
+#     for digit in each_list_2]
+# print(output)
+
+# # Задача 5. Шифр Цезаря
+# print('Задача 5.')
+alphabet = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
+# Функция для шифрования текста по методу Цезаря
+def caesar_cipher(string, user_shift):
+# Создание списка зашифрованных символов
+    char_list = [(alphabet[(alphabet.index(sym) + user_shift) % len(alphabet)]
+         if sym in alphabet else sym)
+        for sym in string]
+# Преобразование списка символов в строку
+    new_str = ''.join(char_list)
+    return new_str
+# Ввод пользователем исходного сообщения и сдвига
+input_str = input('Введите сообщение: ')
+shift = int(input('Введите сдвиг: '))
+# Шифрование сообщения
+output_str = caesar_cipher(input_str, shift)
+# Вывод зашифрованного сообщения
+print('Зашифрованное сообщение:', output_str)
