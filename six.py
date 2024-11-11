dcbfe26cf9c7eb95178e19de8f34451fb48060b0
Osinkin_Vlad <vosinkind@gmail.com>
2024-11-11 12:43:41 +0300

    Решили 3 задачу дз6

--- six.py
+++ six.py
@@ -18,3 +18,13 @@
 # print('Первая команда:', first)
 # print('Вторая команда:', second)
 # print('Победители тура:', win)
+
+# # Задача 3. Двумерный список
+# print('Задача 3.')
+# my_list = [[j_num for j_num in range(i_list, 13, 4)] for i_list in
+# range(1, 5)]
+# print(my_list)
+# # Альтернативный вариант решения с использованием фиксированных смещений
+# second_answer = [[value, value + 4, value + 8] for value in range(1,
+# 5)]
+# print(second_answer)
