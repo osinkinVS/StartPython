001a25d34e5b403951738bb7264b1e7222ce0bf6
Osinkin_Vlad <vosinkind@gmail.com>
2024-11-11 12:24:42 +0300

    Решили первую задачу дз6

---
+++ six.py
@@ -0,0 +1,9 @@
+# # Задача 1. Гласные буквы 
+# print('Задача 1.')
+# text = input('Введите текст: ').lower()
+# glas = 'уеыаоэяиюё'
+# vowels = [i for i in text if i in glas]
+# print('Список гласных букв: ', vowels)
+# print('Длина списка:', len(vowels))
+
+
