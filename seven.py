af938056ef5156e827064d8ae875d9f0e998a4de
Osinkin_Vlad <vosinkind@gmail.com>
2024-11-13 17:29:44 +0300

    Решили дз7

--- seven.py
+++ seven.py
@@ -21,3 +21,25 @@
 # numbers = [1, 2, 3, 4, 5, 6, 7, 8]
 # results: List[Tuple[str, int]] = list(map(lambda x, y: (x, y), strings, numbers))
 # print(results) 
+
+# # Задача 3. Палиндром
+# print('Задача 3.') 
+# from collections import Counter
+# def can_be_poly(val: str) -> bool:
+#     count = Counter(val)
+#     odd_count = len(list(filter(lambda x: x % 2, count.values())))
+#     return odd_count < 2
+# print(can_be_poly('eerru'))
+# print(can_be_poly('abbcba')) 
+# print(can_be_poly('abbbc')) 
+
+# # Задача 4. Уникальный шифр
+# print('Задача 4.') 
+# def unique(string):
+#     lower_str = string.lower()
+#     unique_char = list(filter(lambda c: lower_str.count(c.lower()) == 1, lower_str))
+#     print(unique_char)
+#     return len(unique_char)
+# message = "Today is a beautiful day! The sun is shining and the birds are singing."
+# unique_count = unique(message)
+# print("Количество уникальных символов в строке:", unique_count)
